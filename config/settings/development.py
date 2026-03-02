20c807640e3a5b91a17a00f3bbb6e976931cf7f5
Carolina Scilingo <carol.scilingo2004@gmail.com>
2026-03-01 22:53:21 -0300

    arquivo development.py alterado

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,14 +1,17 @@
 from .base import *
+import environ
 
-SECRET_KEY = 'django-insecure-chave-local-qualquer'
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
 
+SECRET_KEY = env('SECRET_KEY')
 DEBUG = True
-
 ALLOWED_HOSTS = []
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {'default': env.db('DATABASE_URL')}
