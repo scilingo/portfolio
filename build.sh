4c7045c68db3ae9c5ecc300c6e5d14721e1d3604
Carolina Scilingo <carol.scilingo2004@gmail.com>
2026-03-03 22:11:52 -0300

    add: criação automática de superusuário

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; U = get_user_model(); U.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists() or U.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
