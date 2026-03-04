b4006063b504b13b3e3cb02a90003332affa9cc0
Carolina Scilingo <carol.scilingo2004@gmail.com>
2026-03-03 22:42:03 -0300

    add: cloudinary para storage de imagens

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,5 +1,6 @@
 from .base import *
 import environ
+import cloudinary
 
 env = environ.Env()
 environ.Env.read_env(BASE_DIR / '.env')
@@ -27,3 +28,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+INSTALLED_APPS += ['cloudinary', 'cloudinary_storage']
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': env('CLOUDINARY_API_KEY'),
+    'API_SECRET': env('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
