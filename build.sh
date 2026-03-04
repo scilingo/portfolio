6e0654924db7cda552ef6a7a3bdd3f5540630e52
Carolina Scilingo <carol.scilingo2004@gmail.com>
2026-03-03 22:21:16 -0300

    fix: createsuperuser via input

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-echo "from django.contrib.auth import get_user_model; U = get_user_model(); U.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists() or U.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
