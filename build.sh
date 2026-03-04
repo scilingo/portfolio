e38d0989b8aba777b702f47ebdfaad81804c7e7f
Carolina Scilingo <carol.scilingo2004@gmail.com>
2026-03-03 22:29:19 -0300

    remove: createsuperuser do build

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+#python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
